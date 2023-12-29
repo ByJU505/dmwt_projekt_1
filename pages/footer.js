7415ebe0ea62a3074c6864c9e8a818ef37ae0f81
kleinju <justin.klein@student.reutlingen-university.de>
2023-12-29 22:09:22 +0100

    Footer

--- pages/footer.js
+++ pages/footer.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import styles from "../styles/styles.module.css";
+import styles from "../styles/footer.module.css";
+import Image from "next/image";
 function Footer() {
+    const scrollToTop = (event) => {
+        event.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
     return (
         <div className={styles.footer}>
-            <div className={styles.logo}>Logo</div>
+            <div className={styles.logo}>
+                <Image
+                    src={require("../styles/pictures/CloudIT_Logo.png").default}
+                    alt={"Logo"}
+                    width={100}
+                    height={58}
+                    onClick={scrollToTop}
+                    style={{ cursor: 'pointer' }}
+                />
+            </div>
             <div className={styles.navLinks}>
-                <a href="/posts/commentPage">Home</a>
-                <a href="/posts/commentPage">About</a>
-                <a href="/posts/commentPage">Contact</a>
+                <a href="#" style={{ color: '#FFFFFF' }}>Donation</a>
+                <a href="#" style={{ color: '#FFFFFF' }}>About</a>
+                <a href="#" style={{ color: '#FFFFFF' }}>Community</a>
             </div>
         </div>
     );
