04d85c1e2d0ab4ebb2872965c415e0cf0221a83b
kleinju <justin.klein@student.reutlingen-university.de>
2023-12-30 12:06:18 +0100

    Hero3

--- pages/Hero3.js
+++ pages/Hero3.js
@@ -1,11 +1,30 @@
-import styles from "../styles/hero2.module.css";
-import React from 'react';
-
+import styles from "../styles/hero3.module.css";
+import React, { useEffect, useRef } from 'react';
 
 function Hero3() {
+    const scrollRef = useRef(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const threshold = 4000;
+
+            if (scrollPosition >= threshold) {
+                scrollRef.current.classList.add(styles.changedImage);
+            } else {
+                scrollRef.current.classList.remove(styles.changedImage);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div className={styles.container}>
-            <div className={styles.fixedImage}></div>
+            <div className={`${styles.fixedImage} ${styles.defaultImage}`} ref={scrollRef}></div>
             <div className={styles.scrollSection}>
                 <div className={styles.saeule1}></div>
                 <div className={styles.saeule2}></div>
